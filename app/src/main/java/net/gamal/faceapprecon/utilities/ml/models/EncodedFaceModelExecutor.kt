b9413f26e9002d3960139986f341055aba6882ed
Abdelrhman Elgendy <abdelrhman.elgendy@sbs-sa.net>
2024-02-12 14:06:57 +0200

    fix & format.

--- app/src/main/java/net/gamal/faceapprecon/utilities/ml/models/EncodedFaceModelExecutor.kt
+++ app/src/main/java/net/gamal/faceapprecon/utilities/ml/models/EncodedFaceModelExecutor.kt
@@ -3,65 +3,49 @@ package net.gamal.faceapprecon.utilities.ml.models
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Color
 import androidx.lifecycle.LifecycleCoroutineScope
-import kotlinx.coroutines.launch
-import net.gamal.faceapprecon.ml.MobileFaceNet
+import net.gamal.faceapprecon.ml.Mobilenetv2Embedding
 import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 class EncodedFaceModelExecutor(context: Context) {
 
-    private val model = MobileFaceNet.newInstance(context)
+    private val model = Mobilenetv2Embedding.newInstance(context)
 
     fun executeTensorModel(
         lifecycleScope: LifecycleCoroutineScope,
         faceBitmap: Bitmap?,
         onSuccess: (FloatArray) -> Unit
     ) {
-        val inputImageWidth = 224
-        val inputImageHeight = 224
+        if (faceBitmap == null) return
+        val tensorImage = TensorImage(DataType.FLOAT32)
 
-        val resizedBitmap =
-            Bitmap.createScaledBitmap(faceBitmap!!, inputImageWidth, inputImageHeight, true)
+        tensorImage.load(faceBitmap)
 
-        val normalizedBitmap = resizedBitmap.normalizePixelValues()
+        val resized = Bitmap.createScaledBitmap(faceBitmap, 224, 224, true)
 
-        val inputByteBuffer =
-            ByteBuffer.allocateDirect(inputImageWidth * inputImageHeight * 3 * 4) // 4 bytes per float
-        inputByteBuffer.order(ByteOrder.nativeOrder())
-        normalizedBitmap.copyPixelsToBuffer(inputByteBuffer)
-        inputByteBuffer.rewind()
-
-        val inputFeature0 = TensorBuffer.createFixedSize(
-            intArrayOf(1, inputImageWidth, inputImageHeight, 3),
-            DataType.FLOAT32
-        )
-        inputFeature0.loadBuffer(inputByteBuffer)
-
-        lifecycleScope.launch {
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-            val faceEncodedData = outputFeature0.floatArray
-            println("Face encoded data: ${faceEncodedData.contentToString()}")
-            onSuccess(faceEncodedData)
+        val byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3).apply {
+            order(ByteOrder.nativeOrder())
+            val intValues = IntArray(224 * 224)
+            resized.getPixels(intValues, 0, resized.width, 0, 0, resized.width, resized.height)
+            for (pixelValue in intValues) {
+                putFloat(((pixelValue shr 16 and 0xFF) - 127.5f) / 127.5f)
+                putFloat(((pixelValue shr 8 and 0xFF) - 127.5f) / 127.5f)
+                putFloat(((pixelValue and 0xFF) - 127.5f) / 127.5f)
+            }
+            rewind()
         }
-    }
 
-    private fun Bitmap.normalizePixelValues(): Bitmap {
-        val normalizedBitmap = this.copy(this.config, true)
-        val pixels = IntArray(width * height)
-        normalizedBitmap.getPixels(pixels, 0, width, 0, 0, width, height)
-        for (i in pixels.indices) {
-            val pixel = pixels[i]
-            val red = (Color.red(pixel) - 127.5f) / 127.5f
-            val green = (Color.green(pixel) - 127.5f) / 127.5f
-            val blue = (Color.blue(pixel) - 127.5f) / 127.5f
-            pixels[i] = Color.rgb(red.toInt(), green.toInt(), blue.toInt())
-        }
-        normalizedBitmap.setPixels(pixels, 0, width, 0, 0, width, height)
-        return normalizedBitmap
+        val inputFeature0 =
+            TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+        println("executeTensorModel outputFeature0:: ${outputFeature0.floatArray.contentToString()}")
+        onSuccess(outputFeature0.floatArray)
     }
 }
